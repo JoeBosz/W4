034d8839cba1695eb2e2b1bd126eb58498e1cdef
JoeBosz <joeboz2213@gmail.com>
2022-10-27 21:06:25 -0500

    L

--- pages/11-light-dark-toggle/script.js
+++ pages/11-light-dark-toggle/script.js
@@ -1,4 +1,8 @@
-// TODO: Select the HTML element
+const html = document.querySelector("html");
+const checkbox = document.querySelector("input");
 
-// TODO: Add 'dark' to its class list whenever the checkbox is checked
-// HINT: https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+console.log("Hello");
+
+Input.addEventListener("change", function () {
+  html.classList.toggle("dark");
+});
--- pages/12-counter/script.js
+++ pages/12-counter/script.js
@@ -1,2 +1,17 @@
 // TODO: Wire up the buttons to update the count.
 // TODO: Don't allow the count to go below 0.
+
+const buttons = document.querySelectorAll("button");
+const count = document.querySelector("#count");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const countValue = Number(count.textContent);
+    if (event.target.id == "increment") {
+    } else if (event.target.id == "document ") {
+      if (countValue > 0) {
+        count.textContent = countValue - 1;
+      }
+    }
+  });
+});
