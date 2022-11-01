34c18042638903088f4d7eda0869b647d75e9448
JoeBosz <joeboz2213@gmail.com>
2022-10-31 19:06:07 -0500

    Get the form submission as an object

--- pages/14-tip-calc/script.js
+++ pages/14-tip-calc/script.js
@@ -1,3 +1,18 @@
+const { doc } = require("prettier");
+
 // TODO: Get the form submission as an object
+const form = document.querySelector("form");
+
+function handleSumbit(e) {
+  e.preventDefault();
+
+  const submission = {
+    total: e.target.total.value,
+    tip: e.target.tip.value,
+  };
+}
+
+form.addEventListener("submit", handleSumbit);
 // TODO: Write a function to calculate the total amount
+
 // TODO: Output the information showing voff, the tip amount and the total in 'p' tags
